299021eef3136fb28e3436651d25b688739d669d
Aman <amankumar105541@gmail.com>
2026-09-10 22:59:54 +0530

    handling form

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
-app = Flask(__name__,static_folder="assets")
+app = Flask(__name__)    #static_folder="assets"
 
 @app.route("/")
 def hello_world():
-    print(url_for("static", filename="style2.css"))
+    #print(url_for("static", filename="style2.css"))
     return render_template("index.html")
 
 @app.route("/aski")
 def hello():
     return "<p>Hello, LDR...</p>"
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/handle-login",methods=["GET","POST"])
+def handle_login():
+    if request.method=="POST":
+        print(request.form)
+        name=request.form["username"]
+        password=request.form["password"]
+        return f"<p>welcome {name}!</p>"
+        #return "<p>post request</p>"
+    if request.method=="GET":
+        return "<p>get request</p>"
+    return "<p>this is used to handle the form</p>"
 
 
 if __name__=="__main__":
