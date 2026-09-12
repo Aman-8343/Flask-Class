9368825690bb277231b0d507d8cb043198a0e364
Aman <amankumar105541@gmail.com>
2026-09-12 14:06:40 +0530

    query  strings

--- main.py
+++ main.py
@@ -5,27 +5,33 @@ app = Flask(__name__)    #static_folder="assets"
 @app.route("/")
 def hello_world():
     #print(url_for("static", filename="style2.css"))
-    return render_template("index.html")
+    query=request.args.get("q",default="infinix")
+    print(query)
+    return render_template("index.html",query=query)
 
 @app.route("/aski")
 def hello():
     return "<p>Hello, LDR...</p>"
 
-@app.route("/login")
+@app.route("/login",methods=["GET","POST"])
 def login():
-    return render_template("login.html")
-
-@app.route("/handle-login",methods=["GET","POST"])
-def handle_login():
     if request.method=="POST":
-        print(request.form)
         name=request.form["username"]
-        password=request.form["password"]
-        return f"<p>welcome {name}! your pass is {password}</p>"
-        #return "<p>post request</p>"
-    if request.method=="GET":
-        return "<p>get request</p>"
-    return "<p>this is used to handle the form</p>"
+        return f"<p>hello {name}</p>"
+    else:
+        return render_template("login.html")
+
+# @app.route("/handle-login",methods=["GET","POST"])
+# def handle_login():
+#     if request.method=="POST":
+#         print(request.form)
+#         name=request.form["username"]
+#         password=request.form["password"]
+#         return f"<p>welcome {name}! your pass is {password}</p>"
+#         #return "<p>post request</p>"
+#     if request.method=="GET":
+#         return "<p>get request</p>"
+#     return "<p>this is used to handle the form</p>"
 
 
 if __name__=="__main__":
